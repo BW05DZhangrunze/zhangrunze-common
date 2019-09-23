d61dac4df2e0949a8f7b167724a4e95cf4439971
BW05DZhangrunze <2995299523@QQ.com>
2019-09-23 14:33:48 +0800

    工具类项目1.6

--- zhangrunze-common/src/main/java/com/zhangrunze/common/utils/FileUtil.java
+++ zhangrunze-common/src/main/java/com/zhangrunze/common/utils/FileUtil.java
@@ -119,11 +119,43 @@ public class FileUtil {
 		String lineString=null;
 		List  list = new ArrayList();
 		while((lineString =  bufferedReader.readLine()) !=null){
-			String[] split = lineString.split("\\|");
+			String[] split = lineString.split("\\|\\|");
 			Object object = constructor.newInstance(split);
 			list.add(object);
-			
 		}
-		return list;
+		return list;	
+	}
+	
+	
+	/*便利文件夹
+*/	
+	public static List<String> getFileList(String pathName){
+		String[] list = new File(pathName).list();
+		 List<String> fileList = new ArrayList<String>();
+		for (String string : list) {
+			File subFile = new File(pathName + "\\" + string);
+			if(subFile!=null && subFile.exists() && subFile.isFile())
+				fileList.add(pathName + "\\" + string);
+		}
+		return fileList;
+		
+	}
+	
+	/**
+	 * 读取文件内容
+	 * @throws IOException 
+	 */
+	public static String readFile(String fileName) throws IOException {
+
+		StringBuilder sb = new StringBuilder();
+		
+		File file = new File(fileName);
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+		String lineString = null;
+		
+		while ((lineString = bufferedReader.readLine()) != null) {
+			sb.append(lineString).append("\n");
+		}
+		return sb.toString();
 	}
 }
